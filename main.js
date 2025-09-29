face96e6bcbe4fba727063758d2e23f9d23c8458
antaseck27 <antalissa10@gmail.com>
2025-09-29 13:13:44 +0000

    modificationt

---
+++ main.js
@@ -0,0 +1,18 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAQQa-v3Zg-HNwKoNDiTGYrfyXX7SnIiGM",
+    authDomain: "tontine-easy.firebaseapp.com",
+    projectId: "tontine-easy",
+    storageBucket: "tontine-easy.firebasestorage.app",
+    messagingSenderId: "138013449509",
+    appId: "1:138013449509:web:a7e8b5462449f77e7f8ecc"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
