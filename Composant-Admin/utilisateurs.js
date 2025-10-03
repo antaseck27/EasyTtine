6ebdcf05cf9603a1e6c6f1cb2d00e278576afea9
antaseck27 <antalissa10@gmail.com>
2025-10-03 00:12:30 +0000

    ajout sODIFICATION SUR UTILISATEUR

--- Composant-Admin/utilisateurs.js
+++ Composant-Admin/utilisateurs.js
@@ -57,9 +57,20 @@ function renderRow(m) {
     <td class="py-3 px-6 ms-5">${m.progression ?? 0}%</td>
     <td class="py-3 px-6 ms-5">${m.statut ?? "En cours"}</td>
     <td class="py-3 px-6 ms-5 flex gap-3">
-      <button class="hover:text-blue-500">👁</button>
-      <button class="hover:text-yellow-500">✏</button>
-      <button class="hover:text-red-500">🚫</button>
+     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 cursor-pointer hover:text-blue-500">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+        <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+      </svg>
+      <!-- Modifier -->
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 cursor-pointer hover:text-yellow-500">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M9 8.25H7.5a2.25 2.25 0 0 0-2.25 2.25v9a2.25 2.25 0 0 0 2.25 2.25h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25H15M9 12l3 3m0 0 3-3m-3 3V2.25" />
+      </svg>
+      <!-- Bloquer -->
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 cursor-pointer hover:text-red-500">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M18.364 18.364A9 9 0 0 0 5.636 5.636m12.728 12.728A9 9 0 0 1 5.636 5.636m12.728 12.728L5.636 5.636" />
+      </svg>
+      
+   
     </td>
   `;
   return tr;
@@ -75,8 +86,15 @@ function renderRowBloque(m) {
     <td class="py-3 px-6 ms-5">${fmt.format(m.seuil ?? 0)} FCFA</td>
     <td class="py-3 px-6 ms-5 text-red-500 font-bold">${m.statut ?? "Bloqué"}</td>
     <td class="py-3 px-6 ms-5 flex gap-3">
-      <button class="hover:text-blue-500">👁</button>
-      <button class="hover:text-red-500">❌</button>
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 cursor-pointer hover:text-blue-500">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+        <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+      </svg>
+      <!-- Bloquer -->
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 cursor-pointer hover:text-red-500">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M18.364 18.364A9 9 0 0 0 5.636 5.636m12.728 12.728A9 9 0 0 1 5.636 5.636m12.728 12.728L5.636 5.636" />
+      </svg>
+    
     </td>
   `;
   return tr;
